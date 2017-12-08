e8a651c07e1c2951a0548386c5a76b8a5340ed74
Jamie Cooke <jamie@iamjamiecooke.com>
2017-12-08 20:21:12 +0000

    Fix

--- linalg_lstsq_test.py
+++ linalg_lstsq_test.py
@@ -3,11 +3,15 @@ import matplotlib.pyplot as plt
 x = np.array([0, 2, 3, 4, 5])
 y = np.array([1, 2, 2.8, 2.9, 3.6])
 
-A = np.vstack([x, np.ones(len(x))])
+filler = np.empty(len(x))
+filler.fill(1/1.5)
+
+A = np.vstack([x, filler]).T
+print(A)
 
 m, c = np.linalg.lstsq(A, y)[0]
 print(m, c)
 
 plt.plot(x, y, 'o')
 plt.plot(x, m*x + c, 'r')
-plt.show
+plt.show()
